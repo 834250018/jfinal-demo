2a96eb58d67f68c47d6b90ebc203c79b51225026
ve <834250018@qq.com>
2019-12-20 17:51:13 +0800

    1.Dockerfile调整

--- jfinal.sh
+++ jfinal.sh
@@ -41,8 +41,10 @@ fi
 APP_BASE_PATH=$(cd `dirname $0`; pwd)
 CP=${APP_BASE_PATH}/config:${APP_BASE_PATH}/lib/*
 
-    # 运行为后台进程，并在控制台输出信息
-    java -Xverify:none ${JAVA_OPTS} -cp ${CP} ${MAIN_CLASS} &
+function start()
+{
+    # 运行为前台进程，并在控制台输出信息
+    java -Xverify:none ${JAVA_OPTS} -cp ${CP} ${MAIN_CLASS}
 
     # 运行为后台进程，并且不在控制台输出信息
     # nohup java -Xverify:none ${JAVA_OPTS} -cp ${CP} ${MAIN_CLASS} >/dev/null 2>&1 &
@@ -52,3 +54,25 @@ CP=${APP_BASE_PATH}/config:${APP_BASE_PATH}/lib/*
 
     # 运行为非后台进程，多用于开发阶段，快捷键 ctrl + c 可停止服务
     # java -Xverify:none ${JAVA_OPTS} -cp ${CP} ${MAIN_CLASS}
+}
+
+function stop()
+{
+    # 支持集群部署
+    kill `pgrep -f ${APP_BASE_PATH}` 2>/dev/null
+
+    # kill 命令不使用 -9 参数时，会回调 onStop() 方法，确定不需要此回调建议使用 -9 参数
+    # kill `pgrep -f ${MAIN_CLASS}` 2>/dev/null
+
+    # 以下代码与上述代码等价
+    # kill $(pgrep -f ${MAIN_CLASS}) 2>/dev/null
+}
+
+if [[ "$COMMAND" == "start" ]]; then
+	start
+elif [[ "$COMMAND" == "stop" ]]; then
+    stop
+else
+    stop
+    start
+fi
